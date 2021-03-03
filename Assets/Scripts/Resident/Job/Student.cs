c2593822ba1196234517bc1207ec24e60da1d931
Chriswann <chriswann@gmail.com>
2021-03-03 16:27:33 +0100

    Merge Modif

---
+++ Assets/Scripts/Resident/Job/Student.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Student : MonoBehaviour
+{
+    GameObject lastschool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    GameObject FindClosestSchool()
+    {
+        
+        GameObject[] schools = GameObject.FindGameObjectsWithTag("school");
+        if (schools.Length != 0)
+        {
+            lastschool = schools[0];
+            for (int i = 1; i < schools.Length; i++)
+            {
+                if (Vector3.Distance(transform.position, lastschool.transform.position) > Vector3.Distance(transform.position, schools[i].transform.position))
+                {
+                    lastschool = schools[i];
+                }
+            }
+        }
+
+        return lastschool;
+    }
+}
