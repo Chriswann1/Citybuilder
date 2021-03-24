a91ae08903b9caf3da3ebda5dab2ed10ecb0b82e
Chriswann <chriswann@gmail.com>
2021-03-24 16:08:45 +0100

    Camera Movement + Zoom

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private Camera _camera;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float zoomspeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Position cursor " + Input.mousePosition);
+        if (Input.mousePosition.x < 40)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }else if (Input.mousePosition.x > 1880)
+        {
+            transform.Translate(-Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.mousePosition.y < 40)
+        {
+            transform.Translate(-Vector3.up * speed * Time.deltaTime);
+        }else if (Input.mousePosition.y > 1040)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            _camera.fieldOfView -= zoomspeed * Time.deltaTime;
+        }else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            _camera.fieldOfView += zoomspeed * Time.deltaTime;
+        }
     }
 }
