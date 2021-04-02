97377f89de77c06d40c1a49ce15bbada07712fd9
Chriswann <chriswannn@gmail.com>
2021-04-02 14:23:28 +0200

    Library fixed

--- Assets/Scripts/Buildings/Library.cs
+++ Assets/Scripts/Buildings/Library.cs
@@ -7,7 +7,7 @@ public class Library : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameplayManager.Instance.prosperity = GameplayManager.Instance.prosperity + 3;
+       
     }
 
     // Update is called once per frame
